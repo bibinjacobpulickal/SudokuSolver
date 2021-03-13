412b269ec7d345af29a630ac6c52c28b97f87465
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2021-03-14 02:37:37 +0530

    Initial Commit

---
+++ SudokuSolver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SudokuSolver
+//
+//  Created by Bibin Jacob Pulickal on 14/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SudokuSolver/SudokuSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolverApp.swift
+//  SudokuSolver
+//
+//  Created by Bibin Jacob Pulickal on 14/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
